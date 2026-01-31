5bf285dd452ec8258d6d53480da27de71d0d93af
Amit Kumar Singh <amit@sutara.org>
2026-01-31 22:00:33 +0530

    refactor: migrate Next.js image domains configuration from `domains` to `remotePatterns`.

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ["lh3.googleusercontent.com"],
     remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+      },
       {
         protocol: "https",
         hostname: "res.cloudinary.com",
